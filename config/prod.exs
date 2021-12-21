d06f5e597ff5011f8e0174168e935ebf03fc9f1e
Ryan Young <ryoung786@gmail.com>
2021-12-21 02:23:46 -0500

    sendgrid for swoosh emails

--- config/prod.exs
+++ config/prod.exs
@@ -14,47 +14,7 @@ config :strd, StrdWeb.Endpoint, cache_static_manifest: "priv/static/cache_manife
 # Do not print debug messages in production
 config :logger, level: :info
 
-# config :strd, AocWeb.Endpoint,
-#   http: [port: {:system, "PORT"}],
-#   # url: [host: "example.com", port: 80],
-#   url: [scheme: "https", host: "afternoon-castle-16818.herokuapp.com", port: 443],
-#   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-#   # cache_static_manifest: "priv/static/cache_manifest.json",
-#   check_origin: [
-#     "https://strd.ryoung.info",
-#     "https://afternoon-castle-16818.herokuapp.com"
-#   ]
-
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :strd, StrdWeb.Endpoint,
-#       ...,
-#       url: [host: "example.com", port: 443],
-#       https: [
-#         ...,
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-#       ]
-#
-# The `cipher_suite` is set to `:strong` to support only the
-# latest and more secure SSL ciphers. This means old browsers
-# and clients may not be supported. You can set it to
-# `:compatible` for wider support.
-#
-# `:keyfile` and `:certfile` expect an absolute path to the key
-# and cert in disk or a relative path inside priv, for example
-# "priv/ssl/server.key". For all supported SSL configuration
-# options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-#
-# We also recommend setting `force_ssl` in your endpoint, ensuring
-# no data is ever sent via http, always redirecting to https:
-#
-#     config :strd, StrdWeb.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
+# config :strd, Strd.Mailer, adapter: Swoosh.Adapters.Local
+config :strd, Strd.Mailer,
+  adapter: Swoosh.Adapters.Sendgrid,
+  api_key: System.get_env("SENDGRID_API_KEY")
